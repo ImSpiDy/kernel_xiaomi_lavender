0ad272cc627e916b58a8ddc3de334c91cf0a960b
Tyler Nijmeh <tylernij@gmail.com>
2019-06-08 16:56:33 -0700

    block: Force requests onto their origin CPU
    There is a high probability that the specific CPU that makes an I/O
    request may be repeated. For example, the same CPU may read the same
    data multiple times when reading a database. This flag forces requests
    to be completed on the same CPU that requested them.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: ImSpiDy <spidy2713@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -711,9 +711,11 @@ struct request_queue {
 #define QUEUE_FLAG_QUIESCED    28	/* queue has been quiesced */
 
 #define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_SAME_COMP)	|	\
+				 (1 << QUEUE_FLAG_SAME_FORCE)	|	\
 				 (1 << QUEUE_FLAG_WC))
 
 #define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_SAME_COMP)	|	\
+				 (1 << QUEUE_FLAG_SAME_FORCE)	|	\
 				 (1 << QUEUE_FLAG_WC)		|	\
 				 (1 << QUEUE_FLAG_POLL))
 
