dca5cbe495aef9dd0dc27ba45498984fee6290fb
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-25 12:41:56 -0800

    selinux: Remove audit dependency
    Squash of:
     selinux: Remove audit dependency (commit1)
     selinux: don't require auditing (commit2)
     selinux: Allow audit to be disabled (commit3)
    
    commit 1:
      Auditing comes with a lot of overhead due to string assembly via
      vsnprintf. It isn't actually needed to make SELinux work, so remove
      SELinux's artificial dependency on it to make it possible to use SELinux
      without the unneeded overhead.
    
    commit 2:
      Audit is bad. It affects performance under all circumstances. It's also
      dirty.
    
      From LWN: "Andy submitted a patch to fix this particular problem, but he
      didn't stop there. He has come to the conclusion that the audit
      subsystem is beyond repair, so his patch marks the whole thing as being
      broken, making it generally inaccessible. He cited a number of problems
      beyond this security issue: it hurts performance even when it is not
      being used, it is not (in his mind) reliable, it has problems with
      various architectures, and "its approach to freeing memory is
      terrifying." All told, Andy said, we're better off without it"
    
    commit 3:
      SELinux doesn't actually need audit support. In fact, auditing is not
      particularly useful on production kernels, where minimal debugging is
      needed.
    
    Co-authored-by: kdrag0n <dragon@khronodragon.com>
    Co-authored-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -3175,6 +3175,7 @@ static noinline int audit_inode_permission(struct inode *inode,
 					   u32 perms, u32 audited, u32 denied,
 					   int result)
 {
+#ifdef CONFIG_AUDIT
 	struct common_audit_data ad;
 	struct inode_security_struct *isec = inode->i_security;
 	int rc;
@@ -3187,6 +3188,7 @@ static noinline int audit_inode_permission(struct inode *inode,
 			    audited, denied, result, &ad);
 	if (rc)
 		return rc;
+#endif
 	return 0;
 }
 
