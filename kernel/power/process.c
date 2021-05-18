517422b0072737d582a05f15022956195e25e119
Jebaitedneko <Jebaitedneko@gmail.com>
2021-05-19 02:32:09 +0530

    kernel: power: omit trace markers and logspam
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -23,6 +23,9 @@
 #include <trace/events/power.h>
 #include <linux/cpuset.h>
 
+#undef trace_suspend_resume
+#define trace_suspend_resume(x, ...)
+
 /*
  * Timeout for stopping processes
  */
@@ -106,9 +109,6 @@ static int try_to_freeze_tasks(bool user_only)
 				sched_show_task(p);
 		}
 		read_unlock(&tasklist_lock);
-	} else {
-		pr_debug("(elapsed %d.%03d seconds) ", elapsed_msecs / 1000,
-			elapsed_msecs % 1000);
 	}
 
 	return todo ? -EBUSY : 0;
@@ -136,14 +136,11 @@ int freeze_processes(void)
 		atomic_inc(&system_freezing_cnt);
 
 	pm_wakeup_clear(true);
-	pr_debug("Freezing user space processes ... ");
 	pm_freezing = true;
 	error = try_to_freeze_tasks(true);
 	if (!error) {
 		__usermodehelper_set_disable_depth(UMH_DISABLED);
-		pr_debug("done.");
 	}
-	pr_debug("\n");
 	BUG_ON(in_atomic());
 
 	/*
@@ -172,14 +169,8 @@ int freeze_kernel_threads(void)
 {
 	int error;
 
-	pr_debug("Freezing remaining freezable tasks ... ");
-
 	pm_nosig_freezing = true;
 	error = try_to_freeze_tasks(false);
-	if (!error)
-		pr_debug("done.");
-
-	pr_debug("\n");
 	BUG_ON(in_atomic());
 
 	if (error)
@@ -200,8 +191,6 @@ void thaw_processes(void)
 
 	oom_killer_enable();
 
-	pr_debug("Restarting tasks ... ");
-
 	__usermodehelper_set_disable_depth(UMH_FREEZING);
 	thaw_workqueues();
 
@@ -221,7 +210,6 @@ void thaw_processes(void)
 	usermodehelper_enable();
 
 	schedule();
-	pr_debug("done.\n");
 	trace_suspend_resume(TPS("thaw_processes"), 0, false);
 }
 
@@ -230,7 +218,6 @@ void thaw_kernel_threads(void)
 	struct task_struct *g, *p;
 
 	pm_nosig_freezing = false;
-	pr_info("Restarting kernel threads ... ");
 
 	thaw_workqueues();
 
@@ -242,5 +229,4 @@ void thaw_kernel_threads(void)
 	read_unlock(&tasklist_lock);
 
 	schedule();
-	pr_debug("done.\n");
 }
