4cb4fad89772cb8b23ee42ba6ec4e00551b7b28a
Divyanshu-Modi <divyan.m05@gmail.com>
2021-12-07 11:52:24 +0530

    iio: qcom-spmi-adc5: compile out ipc logging conditionally
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/iio/adc/qcom-spmi-adc5.c
+++ drivers/iio/adc/qcom-spmi-adc5.c
@@ -1264,7 +1264,9 @@ static int adc_probe(struct platform_device *pdev)
 	int ret, irq_eoc;
 	u32 reg;
 	bool skip_usb_wa = false;
+#ifdef CONFIG_IPC_LOGGING
 	char adc_name[40];
+#endif
 
 	regmap = dev_get_regmap(dev->parent, NULL);
 	if (!regmap)
