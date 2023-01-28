3ae96422216f029e86392d739e7b066ae3ebaf3d
ImSpiDy <spidy2713@gmail.com>
2023-01-28 09:52:38 +0000

    fs/locks: reduce `lease_break_time` to 20
    Signed-off-by: ImSpiDy <spidy2713@gmail.com>

--- fs/locks.c
+++ fs/locks.c
@@ -154,7 +154,7 @@ static int target_leasetype(struct file_lock *fl)
 }
 
 int leases_enable = 1;
-int lease_break_time = 45;
+int lease_break_time = 20;
 
 /*
  * The global file_lock_list is only used for displaying /proc/locks, so we
