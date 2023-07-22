eab77ff35c42531bf4ad1faca4be672cb1bf1148
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-07-22 12:38:33 +0530

    Revert "selinux: Allow audit to be disabled"
    This reverts commit 1a0893edd51f7cb426becee1a5df1b1a6b8efd68.
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3548,7 +3548,6 @@ out:
 	return match;
 }
 
-#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3571,7 +3570,6 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
-#endif
 
 #ifdef CONFIG_NETLABEL
 /**
