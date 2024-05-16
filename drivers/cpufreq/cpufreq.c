6a2f76793979035af66ab3feb4b14e97b4710c0b
ImSpiDy <SpiDy2713@gmail.com>
2024-05-16 20:55:56 +0530

    Revert "cpufreq: Kill userspace CPU boosting entirely"
    * okay Android 14 you won
    
    This reverts commit 0a8aee0a9a03f031b78c9f42878bfe4772e5e5b9.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -722,10 +722,6 @@ static ssize_t store_##file_name					\
 	int ret, temp;							\
 	struct cpufreq_policy new_policy;				\
 									\
-	if ((&policy->object == &policy->min) &&			\
-		(cpumask_test_cpu(policy->cpu, cpu_perf_mask)))		\
-		return count;						\
-									\
 	memcpy(&new_policy, policy, sizeof(*policy));			\
 	new_policy.min = policy->user_policy.min;			\
 	new_policy.max = policy->user_policy.max;			\
